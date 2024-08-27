5bd047f353ffdf922e7e3270da2d4ad1bb0f4346
oceana <oceanas2027@student.cis.edu.hk>
2024-08-27 13:19:00 +0800

    finish 1st lab

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter your age; ");
+        int age = scanner.nextInt ();
+        int agetwo = age/2+7;
+        System.out.println( age + "-years old olds should date somebody who is at least " + agetwo + "-years old");
+
+
+
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter your age; ");
+        int age = scanner.nextInt ();
+        int agetwo = 100-age;
+        System.out.println( age + "-years old olds should date somebody who is at least " + agetwo + "-years old");
     }
 }
